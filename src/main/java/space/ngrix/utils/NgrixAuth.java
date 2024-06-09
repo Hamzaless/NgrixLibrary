efaa630be47cf974b05fe251996a90abe54de0aa
Hamza Taşdöven <89343438+Hamzaless@users.noreply.github.com>
2024-06-09 14:48:23 +0300

    Update NgrixAuth.java

--- src/main/java/space/ngrix/utils/NgrixAuth.java
+++ src/main/java/space/ngrix/utils/NgrixAuth.java
@@ -8,17 +8,31 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * This class is responsible for checking the license of a plugin.
+ * It communicates with a server to verify the license key and plugin name.
+ */
 public class NgrixAuth {
 
     private String licenseKey;
     private String response;
     private String pluginName;
     private boolean isChecked = false;
+
+    /**
+     * Constructor for the NgrixAuth class.
+     * @param licenseKey The license key to be checked.
+     * @param pluginName The name of the plugin to be checked.
+     */
     public NgrixAuth(String licenseKey, String pluginName){
         this.licenseKey = licenseKey;
         this.pluginName = pluginName;
     }
 
+    /**
+     * Checks the license by sending a POST request to the server.
+     * @return true if the license is valid, false otherwise.
+     */
     public boolean checkLicense(){
         try {
             return postWebContent();
@@ -28,6 +42,11 @@ public class NgrixAuth {
         }
     }
 
+    /**
+     * Sends a POST request to the server with the license key and plugin name.
+     * @return true if the server response contains "success", false otherwise.
+     * @throws Exception if an error occurs during the HTTP request.
+     */
     private boolean postWebContent() throws Exception {
         String url = "https://auth.ngrix.space/license";
 
@@ -59,6 +78,11 @@ public class NgrixAuth {
         return false;
     }
 
+    /**
+     * Returns the server response.
+     * @return the server response as a string.
+     * @throws IllegalStateException if the license has not been checked yet.
+     */
     public String getResponse(){
         if (!isChecked) {
             throw new IllegalStateException("License check has not been done yet.");
@@ -66,21 +90,37 @@ public class NgrixAuth {
         return response;
     }
 
+    /**
+     * Extracts the client name from the server response.
+     * @return the client name as a string.
+     */
     public String getClientName() {
         JSONObject jsonObject = new JSONObject(response);
         return jsonObject.getString("clientName");
     }
 
+    /**
+     * Extracts the end date from the server response.
+     * @return the end date as a string.
+     */
     public String getEndDate() {
         JSONObject jsonObject = new JSONObject(response);
         return jsonObject.getString("endDate");
     }
 
+    /**
+     * Checks if the license is valid by examining the server response.
+     * @return true if the license is valid, false otherwise.
+     */
     public boolean isValid() {
         JSONObject jsonObject = new JSONObject(response);
         return jsonObject.getBoolean("isValid");
     }
 
+    /**
+     * Extracts the number of days left until the license expires from the server response.
+     * @return the number of days left as an integer.
+     */
     public Integer getDaysLeft() {
         JSONObject jsonObject = new JSONObject(response);
         return jsonObject.getInt("daysLeft");
